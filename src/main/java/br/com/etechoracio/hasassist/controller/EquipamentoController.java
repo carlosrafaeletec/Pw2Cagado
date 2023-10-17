4f076f1429adcbac4174607cb61952c9b3d65d97
carlo e rafa <carlosrafaelgit@gmail.com>
2023-10-17 09:03:05 -0300

    Jpa2

--- src/main/java/br/com/etechoracio/hasassist/controller/EquipamentoController.java
+++ src/main/java/br/com/etechoracio/hasassist/controller/EquipamentoController.java
@@ -1,10 +1,31 @@
 package br.com.etechoracio.hasassist.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.etechoracio.hasassist.entity.Equipamento;
+import br.com.etechoracio.hasassist.repository.EquipamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/equipamentos")
 public class EquipamentoController {
+    @Autowired
+    private EquipamentoRepository reposity;
 
+    @GetMapping
+    public List<Equipamento> listarTodos() {
+        return reposity.findAll();
+    }
+
+    @PostMapping
+    public Equipamento inserir(@RequestBody Equipamento body){
+        return reposity.save(body);
+    }
+
+    @GetMapping
+    public List<Equipamento> listar(@PathVariable Long id){
+        return reposity.findById(id);
+    }
 }
